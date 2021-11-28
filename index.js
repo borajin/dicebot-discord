7fc2cc79dc5f921f8bb8fefb26780f82776d1a4f
borajin <borajin.soft@gmail.com>
2021-11-29 00:49:21 +0900

    procfile 추가

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const config = require("./config.json");
-
 const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 const lodash = require('lodash');
