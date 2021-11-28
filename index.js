ca71541369e6b8d359847c7e45295187c3afaf10
borajin <borajin.soft@gmail.com>
2021-11-28 23:04:14 +0900

    login

---
+++ index.js
@@ -0,0 +1,5 @@
+const config = require("./config.json");
+
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+client.login(config.BOT_TOKEN);
