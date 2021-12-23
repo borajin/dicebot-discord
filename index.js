2bd4af731fc9cca77a6987a3493fe193da24ff3d
borajin <borajin.soft@gmail.com>
2021-12-23 15:06:48 +0900

    정규식 사용해서 수정

--- index.js
+++ index.js
@@ -1,58 +1,89 @@
-const { Client, Intents, MessageEmbed } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
-const lodash = require('lodash');
+const { Client, Intents, MessageEmbed } = require("discord.js");
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+});
+const lodash = require("lodash");
 
 const prefix = `/r `;
 
+const solveDexp = (dExp) => {
+  const args = dExp.split(/d/i);
+  const results = [];
 
-function randomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-}
+  for (let i = 0; i < Number(args[0]); i++) {
+    const rand = randomInt(1, Number(args[1]));
+    results.push(rand);
+  }
 
-client.on('ready', () => {
-    client.user.setActivity('/r 2d6<10 처럼 사용하세요!!', { type: 'WATCHING' });
-})
+  return results;
+};
+
+const randomInt = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+client.on("ready", () => {
+  client.user.setActivity("/r 2d6<10 처럼 사용하세요!!", { type: "WATCHING" });
+});
 
 client.on("message", function (message) {
+  // message 작성자가 봇이면 그냥 return
+  if (message.author.bot) return;
+  // message 시작이 prefix가 아니면 return
+  if (!message.content.startsWith(prefix)) return;
 
-    // message 작성자가 봇이면 그냥 return
-    if (message.author.bot) return;
-    // message 시작이 prefix가 아니면 return
-    if (!message.content.startsWith(prefix)) return;
+  //수식과 텍스트 분리
+  const command = message.content.slice(prefix.length).replace(" ", "").trim();
 
-    //수식과 텍스트 분리
-    const commandBody = message.content.slice(prefix.length);
-    const args = commandBody.split(' ');
-    const diceCommand = args[0]; //수식
+  if (command.match(/<|>/g).length === 1) {
+    const commandArgs = command.split(/<|>/);
 
-    //d 인지 D 인지 
-    const dice = diceCommand.includes('d') ? 'd' : diceCommand.includes('D') ? 'D' : 'null';
-    const diceArgs = diceCommand.split(dice); //1은 회수, 2는 숫자
+    if (
+      !commandArgs[0].replace(/\+|-|\*|\/|d|D|\d/g, "") === "" ||
+      commandArgs[1].match(/^\d*/)[0] === ""
+    ) {
+      message.channel.send(
+        "사용법 : /r 2d5+2d6<10 text 와 같이 부등호는 한개, 부등호 앞은 수식, 부등호 뒤는 숫자를 넣어주세요."
+      );
+    } else {
+      const sign = command.match(/<|>/)[0];
+      const oriDexps = commandArgs[0].match(/\d*d\d*/gi);
+      const signNum = commandArgs[1].match(/^\d*/)[0];
+      const text = commandArgs[1].slice(signNum.length);
 
-    //부등호 추출
-    const sign = diceArgs[1].includes('<') ? '<' : diceArgs[1].includes('>') ? '>' : '';
-    let number = sign !== '' ? diceArgs[1].split(sign)[1] : '';
+      //수식 계산
+      const resutls = oriDexps.map((item, index) => {
+        const resultArr = solveDexp(item);
 
-    //난수 생성
-    const result = [];
-    let successText = '';
+        commandArgs[0] = commandArgs[0].replace(
+          oriDexps[index],
+          resultArr.reduce((sum, curValue) => sum + curValue)
+        );
 
-    for (let i = 0; i < parseInt(diceArgs[0] == '' ? '1' : diceArgs[0]); i++) {
-        const rand = randomInt(parseInt(diceArgs[0]), parseInt(diceArgs[1]));
-        result.push(rand);
+        return resultArr;
+      });
+
+      const diceResult = Math.round(eval(commandArgs[0]));
+
+      if (sign === "<")
+        successText = diceResult <= Number(signNum) ? "success" : "fail";
+      else if (sign === ">")
+        successText = diceResult >= Number(signNum) ? "success" : "fail";
+
+      let colorCode = "#" + Math.round(Math.random() * 0xffffff).toString(16);
+      const embed = new MessageEmbed()
+        .setColor(colorCode)
+        .setTitle(message.content)
+        .setDescription(`(${resutls})\n= ${diceResult} ${successText}`)
+        .setAuthor(message.author.username, message.author.displayAvatarURL());
+
+      message.channel.send({ embeds: [embed] });
     }
-
-    const sum = lodash.sum(result);
-
-    if (sign == '<') successText = sum <= number ? "success" : "fail";
-    else if (sign == '>') successText = sum >= number ? "success" : "fail";
-
-
-    let colorCode = "#" + Math.round(Math.random() * 0xffffff).toString(16);
-    const embed = new MessageEmbed().setColor(colorCode).setTitle(message.content).setDescription(`(${result.join('+')})\n= ${sum} ${successText}`).setAuthor(message.author.username, message.author.displayAvatarURL())
-
-    message.channel.send({ embeds: [embed] });
-
+  } else {
+    message.channel.send(
+      "사용법 : /r 2d5+2d6<10 text 와 같이 부등호는 한개, 부등호 앞은 수식, 부등호 뒤는 숫자를 넣어주세요."
+    );
+  }
 });
 
 client.login(process.env.BOT_TOKEN);
